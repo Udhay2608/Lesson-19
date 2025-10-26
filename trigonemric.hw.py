972bec8c5ef2068c41590546bd714ab750cec59a
Udhay2608 <U-Tiwana23@st-bernards.slough.sch.uk>
2025-10-26 09:17:45 +0000

    Add files via upload

---
+++ trigonemric.hw.py
@@ -0,0 +1,7 @@
+import math
+a=float(input("Angle Degrees "))
+r=math.radians(a)
+print(f"sin{a})={math.sin(r):.4f}")
+print(f"cos{a})={math.cos(r):.4f}")
+print(f"tan{a})={math.tan(r):.4f}")
+print("best of luck.harina")
